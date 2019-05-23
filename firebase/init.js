9da4913cdd50c83e3a23e1b1efb893e0e730adcc
Varadsingh Pardeshi <varadsingh@hotmail.com>
2019-05-23 20:31:36 +0530

    Add files via upload

---
+++ firebase/init.js
@@ -0,0 +1,10 @@
+var config = {
+	apiKey: "your apiKey",
+    authDomain: "your authDomain",
+    databaseURL: "your databdatabaseURLaseurl",
+    projectId: "your projectId",
+    storageBucket: "your storageBucket",
+    messagingSenderId: "your messagingSenderId",
+    appId: "your appId"
+};
+firebase.initializeApp(config);
